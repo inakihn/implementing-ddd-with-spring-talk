69dfb753ef923c614a09db474caa83ca8d61970a
inakihn <18142467+inakihn@users.noreply.github.com>
2024-06-20 10:12:07 +0200

    add testcontainers to test

--- src/test/java/library/LibraryApplicationTests.java
+++ src/test/java/library/LibraryApplicationTests.java
@@ -3,9 +3,28 @@ package library;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class LibraryApplicationTests {
+
+    static final PostgreSQLContainer<?> POSTGRES;
+
+    static {
+        POSTGRES = new PostgreSQLContainer<>("postgres:16-alpine");
+        POSTGRES.withReuse(true)
+                .withLabel("reuse.UUID", "41c3ccbc-34b6-46f4-892d-ed05379348c2");
+        POSTGRES.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
 
     @Test
     void contextLoads() {
