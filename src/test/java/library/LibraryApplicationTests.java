d3120c2a7a27652b1d5ee2b27895f058cefdadca
inakihn <18142467+inakihn@users.noreply.github.com>
2024-07-01 09:29:55 +0200

    add example of an integration test

--- src/test/java/library/LibraryApplicationTests.java
+++ src/test/java/library/LibraryApplicationTests.java
@@ -1,6 +1,10 @@
 package library;
 
+import library.catalog.application.AddBookToCatalogUseCase;
+import library.catalog.domain.BookRepository;
+import library.catalog.domain.Isbn;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -9,6 +13,9 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @SpringBootTest
 class LibraryApplicationTests {
 
+    private final AddBookToCatalogUseCase addBookToCatalogUseCase;
+    private final BookRepository bookRepository;
+
     static final PostgreSQLContainer<?> POSTGRES;
 
     static {
@@ -16,6 +23,13 @@ class LibraryApplicationTests {
         POSTGRES.withReuse(true)
                 .withLabel("reuse.UUID", "41c3ccbc-34b6-46f4-892d-ed05379348c2");
         POSTGRES.start();
+    }
+
+    @Autowired
+    LibraryApplicationTests(AddBookToCatalogUseCase addBookToCatalogUseCase,
+                            BookRepository bookRepository) {
+        this.addBookToCatalogUseCase = addBookToCatalogUseCase;
+        this.bookRepository = bookRepository;
     }
 
     @DynamicPropertySource
